74fe9e1dfcac3cf6e57b6aee344e8f076cb34077
jasgalani12 <galanijas12@gmail.com>
2024-07-16 08:30:43 +0530

    lab 3 added

---
+++ lab3/a.java
@@ -0,0 +1,6 @@
+//write a program to display your name in command promot with double quote
+class a {
+    public static void main(String[] args) {
+        System.out.println("\"jas\"");
+    }
+}
---
+++ lab3/b.java
@@ -0,0 +1,13 @@
+//write a program then input 2 integer value and get output with swap value (note* using 3rd value)
+class b {
+    public static void main(String[] args) {
+        int x=4,y=2,z;
+        System.out.println("value of x="+x);
+        System.out.println("value of y="+y);
+        z=x;
+        x=y;
+        y=z;
+        System.out.println("value of x="+x);
+        System.out.println("value of y="+y);
+    }
+}
